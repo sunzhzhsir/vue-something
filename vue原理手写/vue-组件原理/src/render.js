363a8a376c7a1000bae4a46191d8f15da25da354
sunsir12138 <2804303347@qq.com>
2022-09-28 14:11:39 +0800

    修改

---
+++ vue原理手写/vue-组件原理/src/render.js
@@ -0,0 +1,35 @@
+import { createElement, createTextNode } from "./vdom/index.js";
+import { nextTick } from "./util/next-tick.js";
+
+export function renderMixin(Vue) {
+  Vue.prototype._render = function () {
+    const vm = this;
+    // 获取模板编译生成的render方法
+    const { render } = vm.$options;
+    // 生成vnode--虚拟dom
+    const vnode = render.call(vm);
+
+    return vnode;
+  };
+  // 挂载在原型的nextTick方法 可供用户手动调用
+  Vue.prototype.$nextTick = nextTick;
+
+  // render函数里面有_c _v _s方法需要定义
+  Vue.prototype._c = function (...args) {
+    // 创建虚拟dom元素
+    return createElement(this, ...args);
+  };
+
+  Vue.prototype._v = function (text) {
+    // 创建虚拟dom文本
+    return createTextNode(text);
+  };
+  Vue.prototype._s = function (val) {
+    // 如果模板里面的是一个对象  需要JSON.stringify
+    return val == null
+      ? ""
+      : typeof val === "object"
+      ? JSON.stringify(val)
+      : val;
+  };
+}
